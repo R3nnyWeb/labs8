606e4f6955adafa87c7ad9a8c686dcae79675676
Nikita Vashkulatov <vashkulatov.nikita@promregion.net>
2024-02-14 22:26:37 +0300

    lab 3 front

--- InfoSecurity/lab1/src/main/kotlin/Main.kt
+++ InfoSecurity/lab1/src/main/kotlin/Main.kt
@@ -1,19 +1,23 @@
+import cardano.CardanoController
 import io.javalin.Javalin
 
 fun main() {
-    val app = Javalin.create{ config ->
-    config.bundledPlugins.enableCors { cors ->
-        cors.addRule {
-            it.anyHost()
+    val app = Javalin.create { config ->
+        config.bundledPlugins.enableCors { cors ->
+            cors.addRule {
+                it.anyHost()
+            }
         }
     }
-}
         .get("/caesar/encrypt", CaesarController.encrypt)
         .get("/caesar/decrypt", CaesarController.decrypt)
         .get("/caesar/decrypt-force", CaesarController.tryDecrypt)
+        .post("/cardano/encrypt", CardanoController.encrypt)
+        .post("/cardano/decrypt", CardanoController.decrypt)
         .exception(Exception::class.java) { e, ctx ->
             ctx.status(500)
             ctx.result(e.message ?: "Unknown error")
+            e.printStackTrace()
         }
         .start(7070)
 }
---
+++ InfoSecurity/lab1/src/main/kotlin/cardano/CardanoRequest.kt
@@ -0,0 +1,6 @@
+package cardano
+
+data class CardanoRequest(
+    val grid: Array<BooleanArray>,
+    val source: String
+)
