c769022084b6e2b9a442f35f2d6f4226cf96e234
Slava <Anohin.VA@yandex.ru>
2024-02-28 17:59:00 +0300

    postman

--- MSIPU/Lab3/ex1.s
+++ MSIPU/Lab3/ex1.s
@@ -7,10 +7,14 @@ stack_top
 	dcd start			
 	area program,code,readonly	
 	entry				
-start					
+start	
+	mov r9, #0xfff
+tau10	subs r9, #1
+	bpl tau10		
+
 	bl		pp1		;
 	bl		pp2		;
-m	bl		pp3		;
+m;	bl		pp3		;
 ;	bl		pp4		;
 ;	bl		pp5		;
 	b		m		;
@@ -46,54 +50,58 @@ pp1
 	mov	r7, #0x08		;	_DAC_
 	str	r7, [r6]		;	
 	ldr r7,=0x0aa4c010	; per_clock
-	str r7, [r0, #0x1c]	;	ldr  r7, =0x03000000;Tim-1-2-Clk
+	
+	str r7, [r0, #0x1c]	
+	ldr  r7, =0x03000000;Tim-1-2-Clk
 	str r7, [r0, #0x24]
 	
-	mov  r7,  # 0xA		;PSG _Tim1, Tim2_
+	mov  r7,  # 0xff		;PSG _Tim1, Tim2_
 	str  r7, [r1, #0x04]; 
 	str  r7, [r2, #0x04]; 
-	movw  r7,  #100		;ARR
+	;;;;;;;;;;;;;;;;;;;;;;;;
+	mov r7, #0x1
+	str r7, [r1]
+	;;;;;;;;;;;;;;;;;;;;;;;;
+	movw  r7,  #0xff	;ARR
 	str  r7, [r1, #0x08]; 
 	str  r7, [r2, #0x08]; 
 	mov  r7, #0x01		;CNTRL
 	str  r7, [r1, #0x0c];
 	str  r7, [r2, #0x0c];
-;	movw  r7, #25		;ch1_CCR	
-;	str  r7, [r1, #0x10];
-; 	str  r7, [r2, #0x10];
-;	movw  r7, #15		;ch2_CCR	
-;	str  r7, [r1, #0x14];
-; 	str  r7, [r2, #0x14];
-;	mov  r7, # 0x0c00	;CH1_CNTRL
-;	str  r7,[r1, #0x20]	;
-;	str  r7,[r2, #0x20]	;
-;	mov  r7, # 0x0c00	;CH2_CNTRL
-;	str  r7,[r1, #0x24]	;
-;	str  r7,[r2, #0x24]	;
-;	movw  r7, # 0x909	;CH1_CNTRL1;		
-;	str  r7, [r1, #0x30];  
-;	str  r7, [r2, #0x30]; 
-;	movw r0, #0x909		;CH2_CNTRL1;
-;	str  r0, [r1, #0x34];
-;	str  r0, [r2, #0x34];*/
+	movw  r7, #25		;ch1_CCR	
+	str  r7, [r1, #0x10];
+ 	str  r7, [r2, #0x10];
+	movw  r7, #15		;ch2_CCR	
+	str  r7, [r1, #0x14];
+ 	str  r7, [r2, #0x14];
+	mov  r7, # 0x0c00	;CH1_CNTRL
+	str  r7,[r1, #0x20]	;if <25 then 1 else 0  on 1 pin
+	str  r7,[r2, #0x20]	; if <  15 on pin3
+	mov  r7, # 0x0c00	;CH2_CNTRL
+	str  r7,[r1, #0x24]	; same
+	str  r7,[r2, #0x24]	;
+	movw  r7, # 0x909	;CH1_CNTRL1;		
+	str  r7, [r1, #0x30];  
+	str  r7, [r2, #0x30]; 
+	movw r0, #0x909		;CH2_CNTRL1;
+	str  r0, [r1, #0x34];
+	str  r0, [r2, #0x34];
 	bx    lr
 pp2	
-;	movw r7, #0x03b8
-	mov r7, #0		
+	movw r7, #0x03b8
+;	mov r7, #0		
 	str   r7, [r3,#0x08]; _FUNC
 	bx    lr
 pp3	
-	ldr r10, [r1]
-	str r10, [r3]
-;	add	r11, #0x01	
-;	and r12, r11, 0xff
-;	add r12,  0x0; N(t)
-;	str r12,[r3]			
-;	str r12,[r6,#0x08]
-;	str r12,[r1,#0x05]
-;	str r11, [r2,#0x10]
-;	mov r9, #0xfff
-;tau	subs r9, #1
-;	bpl tau		 */
+	add	r11, #0x01	
+	and r12, r11, 0xff
+	add r12,  0x0; N(t)
+	str r12,[r3]			
+	str r12,[r6,#0x08]
+	str r12,[r1,#0x05]
+	str r11, [r2,#0x10]
+	mov r9, #0xfff
+tau	subs r9, #1
+	bpl tau		 
 	bx    lr	
 	END		
